142cec38328678657d153a55c0a1fc7ca17d2160
zouyuanle2021 <lrunjie0824@sina.com>
2022-12-15 23:06:09 +0800

    feat: 歌手接口

---
+++ src/service/api/singer/index.ts
@@ -0,0 +1,99 @@
+import { stringifyParams } from "@/utils"
+import http from "../../http"
+import { CollectorCancelSingerParam, CollectorCancelSingerResult, SimilarSingerResult, SingelSingerSongParam, SingelSingerSongResult, SingerAlbumParam, SingerAlbumresult, SingerCategoryParam, SingerCategoryResult, SingerDesResult, SingerFansParam, SingerFansResult, SingerMvResult, SingerTopSongResult } from "./types"
+
+
+/**
+ * 获取歌手单曲，可获得歌手部分信息和热门歌曲
+ * @param params
+ * id: 歌手id
+ * @returns
+ */
+export const getSingelSingerSong = (params: SingelSingerSongParam) => {
+    return http.get<SingelSingerSongResult>(`/artists${stringifyParams(params)}`)
+}
+
+/**
+ * 获取歌手 mv
+ * @param params
+ * id: 歌手id
+ * @returns
+ */
+export const getSingerMv = (params: SingelSingerSongParam) => {
+    return http.get<SingerMvResult>(`/artist/mv${stringifyParams(params)}`)
+}
+
+
+/**
+ * 获取歌手专辑
+ * @param params
+ * id: 歌手id
+ * 可选参数 : limit: 取出数量 , 默认为 30
+ * offset: 偏移数量 , 用于分页 , 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认 为 0
+ * @returns
+ */
+export const getSingerAlbum = (params: SingerAlbumParam) => {
+    return http.get<SingerAlbumresult>(`/artist/album${stringifyParams(params)}`)
+}
+
+/**
+ * 获取歌手描述
+ * @param params
+ * id: 歌手id
+ * @returns
+ */
+export const getSingerDes = (params: SingelSingerSongParam) => {
+    return http.get<SingerDesResult>(`/artist/desc${stringifyParams(params)}`)
+}
+
+/**
+ * 获取相似歌手
+ * @param params
+ * @returns
+ */
+export const getSimilarSinger = (params: SingelSingerSongParam) => {
+    return http.get<SimilarSingerResult>(`/simi/artist${stringifyParams(params)}`)
+}
+
+
+/**
+ * 歌手分类列表
+ * @param params
+ * limit : 返回数量 , 默认为 30
+ * offset : 偏移数量，用于分页 , 如 : 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+ * initial: 按首字母索引查找参数,如 /artist/list?type=1&area=96&initial=b 返回内容将以 name 字段开头为 b 或者拼音开头为 b 为顺序排列, 热门传-1,#传 0
+ * type: -1:全部 1:男歌手 2:女歌手 3:乐队
+ * area: -1:全部 7华语 96欧美 8:日本 16韩国 0:其他
+ * @returns
+ */
+export const getSingerByCategory = (params: SingerCategoryParam) => {
+    return http.get<SingerCategoryResult>(`/artist/list${stringifyParams(params)}`)
+}
+
+/**
+ * 收藏/取消收藏 歌手
+ * @param params
+ * id : 歌手 id
+ * t:操作,1 为收藏,其他为取消收藏
+ * @returns
+ */
+export const collectOrCancelSinger = (params: CollectorCancelSingerParam) => {
+    return http.get<CollectorCancelSingerResult>(`/artist/sub${stringifyParams(params)}`)
+}
+
+export const getSingerTopSong = (params: SingelSingerSongParam) => {
+    return http.get<SingerTopSongResult>(`/artist/top/song${stringifyParams(params)}`)
+}
+
+// 收藏的歌手列表 TODO
+
+
+/**
+ * 歌手粉丝
+ * @param params
+ * @returns
+ */
+export const getSingerFans = (params: SingerFansParam) => {
+    return http.get<SingerFansResult>(`/artist/fans${stringifyParams(params)}`)
+}
+
---
+++ src/service/api/singer/types.ts
@@ -0,0 +1,500 @@
+import { profile } from "../login/types";
+
+// 歌手部分信息和热门歌曲
+export interface SingelSingerSongParam {
+    id: number
+}
+export interface Artist {
+    img1v1Id: number;
+    topicPerson: number;
+    img1v1Url: string;
+    followed?: boolean;
+    picUrl: string;
+    alias: string[];
+    picId: number;
+    musicSize: number;
+    albumSize: number;
+    briefDesc: string;
+    trans: string;
+    name: string;
+    id: number;
+    publishTime?: number;
+    picId_str?: string;
+    img1v1Id_str: string;
+    mvSize?: number;
+    transNames?: any;
+    identifyTag?: any;
+    alg?: string;
+    fansCount?: number;
+    isSubed?: any;
+    accountId?: number;
+    showPrivateMsg?: any;
+}
+
+export interface Ar {
+    id: number;
+    name: string;
+    alia: string[];
+}
+
+export interface Al {
+    id: number;
+    name: string;
+    picUrl: string;
+    pic_str: string;
+    pic: number;
+}
+
+export interface H {
+    br: number;
+    fid: number;
+    size: number;
+    vd: number;
+    sr: number;
+}
+
+export interface L {
+    br: number;
+    fid: number;
+    size: number;
+    vd: number;
+    sr: number;
+}
+
+export interface Sq {
+    br: number;
+    fid: number;
+    size: number;
+    vd: number;
+    sr: number;
+}
+
+export interface M {
+    br: number;
+    fid: number;
+    size: number;
+    vd: number;
+    sr: number;
+}
+
+export interface FreeTrialPrivilege {
+    resConsumable: boolean;
+    userConsumable: boolean;
+    listenType?: any;
+}
+
+export interface ChargeInfoList {
+    rate: number;
+    chargeUrl?: any;
+    chargeMessage?: any;
+    chargeType: number;
+}
+
+export interface Privilege {
+    id: number;
+    fee: number;
+    payed: number;
+    st: number;
+    pl: number;
+    dl: number;
+    sp: number;
+    cp: number;
+    subp: number;
+    cs: boolean;
+    maxbr: number;
+    fl: number;
+    toast: boolean;
+    flag: number;
+    preSell: boolean;
+    playMaxbr: number;
+    downloadMaxbr: number;
+    maxBrLevel: string;
+    playMaxBrLevel: string;
+    downloadMaxBrLevel: string;
+    plLevel: string;
+    dlLevel: string;
+    flLevel: string;
+    rscl?: any;
+    freeTrialPrivilege: FreeTrialPrivilege;
+    chargeInfoList: ChargeInfoList[];
+}
+
+export interface HotSong {
+    rtUrls: any[];
+    ar: Ar[];
+    al: Al;
+    st: number;
+    noCopyrightRcmd?: any;
+    songJumpInfo?: any;
+    cd: string;
+    t: number;
+    v: number;
+    h: H;
+    l: L;
+    sq: Sq;
+    hr?: any;
+    mv: number;
+    no: number;
+    fee: number;
+    djId: number;
+    rtype: number;
+    rurl?: any;
+    mst: number;
+    cp: number;
+    cf: string;
+    dt: number;
+    crbt?: any;
+    rtUrl?: any;
+    ftype: number;
+    pst: number;
+    alia?: any[];
+    pop: number;
+    rt: string;
+    a?: any;
+    m: M;
+    name: string;
+    id: number;
+    privilege: Privilege;
+    eq: string;
+}
+export interface SingelSingerSongResult {
+    artist: Artist;
+    hotSongs: HotSong[];
+    more: boolean;
+    message?: string;
+    code: number;
+}
+
+
+// 获取歌手 mv
+export interface Mv {
+    id: number;
+    name: string;
+    status: number;
+    artist: Artist;
+    imgurl: string;
+    imgurl16v9: string;
+    artistName: string;
+    duration: number;
+    playCount: number;
+    publishTime: string;
+    subed: boolean;
+}
+export interface SingerMvResult {
+    mvs: Mv[];
+    time: number;
+    hasMore: boolean;
+    code: number;
+}
+
+
+
+// 获取歌手专辑
+export interface SingerAlbumParam {
+    id: number
+    limit?: number
+    offset?: number
+}
+
+export interface HotAlbum {
+    songs: any[];
+    paid: boolean;
+    onSale: boolean;
+    mark: number;
+    awardTags?: any;
+    companyId: number;
+    blurPicUrl: string;
+    alias: any[];
+    artists: Artist[];
+    copyrightId: number;
+    picId: number;
+    artist: Artist;
+    briefDesc: string;
+    publishTime: number;
+    company: string;
+    picUrl: string;
+    commentThreadId: string;
+    pic: number;
+    description: string;
+    tags: string;
+    status: number;
+    subType: string;
+    name: string;
+    id: number;
+    type: string;
+    size: number;
+    picId_str: string;
+}
+
+export interface SingerAlbumresult {
+    artist: Artist;
+    hotAlbums: HotAlbum[];
+    more: boolean;
+    code: number;
+}
+
+
+// 获取歌手描述
+export interface Introduction {
+    ti: string;
+    txt: string;
+}
+
+export interface Content {
+    type: number;
+    id: number;
+    content: string;
+}
+
+export interface Topic {
+    id: number;
+    addTime: number;
+    mainTitle: string;
+    title: string;
+    content: Content[];
+    userId: number;
+    cover: number;
+    headPic: number;
+    shareContent: string;
+    wxTitle: string;
+    showComment: boolean;
+    status: number;
+    seriesId: number;
+    pubTime: number;
+    readCount: number;
+    tags: string[];
+    pubImmidiatly: boolean;
+    auditor: string;
+    auditTime: number;
+    auditStatus: number;
+    startText: string;
+    delReason: string;
+    showRelated: boolean;
+    fromBackend: boolean;
+    rectanglePic: number;
+    updateTime: number;
+    reward: boolean;
+    summary: string;
+    memo?: any;
+    adInfo: string;
+    categoryId: number;
+    hotScore: number;
+    recomdTitle: string;
+    recomdContent: string;
+    number: number;
+}
+
+export interface Creator {
+    userId: number;
+    userType: number;
+    nickname: string;
+    avatarImgId: number;
+    avatarUrl: string;
+    backgroundImgId: number;
+    backgroundUrl: string;
+    signature: string;
+    createTime: number;
+    userName: string;
+    accountType: number;
+    shortUserName: string;
+    birthday: number;
+    authority: number;
+    gender: number;
+    accountStatus: number;
+    province: number;
+    city: number;
+    authStatus: number;
+    description: string;
+    detailDescription: string;
+    defaultAvatar: boolean;
+    expertTags?: any;
+    experts?: any;
+    djStatus: number;
+    locationStatus: number;
+    vipType: number;
+    followed: boolean;
+    mutual: boolean;
+    authenticated: boolean;
+    lastLoginTime: number;
+    lastLoginIP: string;
+    remarkName?: any;
+    viptypeVersion: number;
+    authenticationTypes: number;
+    avatarDetail?: any;
+    anchor: boolean;
+}
+
+export interface TopicData {
+    topic: Topic;
+    creator: Creator;
+    shareCount: number;
+    commentCount: number;
+    likedCount: number;
+    liked: boolean;
+    rewardCount: number;
+    rewardMoney: number;
+    relatedResource?: any;
+    rectanglePicUrl: string;
+    coverUrl: string;
+    categoryId: number;
+    categoryName: string;
+    reward: boolean;
+    memo?: any;
+    recmdTitle: string;
+    recmdContent: string;
+    shareContent: string;
+    wxTitle: string;
+    addTime: number;
+    seriesId: number;
+    showComment: boolean;
+    showRelated: boolean;
+    summary: string;
+    title: string;
+    mainTitle: string;
+    commentThreadId: string;
+    readCount: number;
+    url: string;
+    tags: string[];
+    id: number;
+    number: number;
+}
+
+export interface SingerDesResult {
+    introduction: Introduction[];
+    briefDesc: string;
+    count: number;
+    message?: string;
+    topicData: TopicData[];
+    code: number;
+}
+
+// 获取相似歌手
+
+export interface SimilarSingerResult {
+    artists: Artist[];
+    message?: string
+    code: number;
+}
+
+
+// 歌手分类列表
+export interface SingerCategoryParam {
+    limit?: number
+    offset?: number
+    initial?: string
+    type: -1 | 1 | 2 | 3
+    area: -1 | 7 | 96 | 8 | 16 | 0
+}
+export interface SingerCategoryResult {
+    artists: Artist[];
+    more: boolean;
+    message?: string;
+    code: number;
+}
+
+// 收藏/取消收藏歌手
+
+export interface CollectorCancelSingerParam {
+    id: number
+    t: number
+}
+// TODO
+export interface CollectorCancelSingerResult {
+    code: number
+}
+
+// 歌手热门 50 首歌曲
+export interface Song {
+    name: string;
+    id: number;
+    pst: number;
+    t: number;
+    ar: Ar[];
+    alia?: any[];
+    pop: number;
+    st: number;
+    rt: string;
+    fee: number;
+    v: number;
+    crbt?: any;
+    cf: string;
+    al: Al;
+    dt: number;
+    h: H;
+    m: M;
+    l: L;
+    sq: Sq;
+    hr?: any;
+    a?: any;
+    cd: string;
+    no: number;
+    rtUrl?: any;
+    ftype: number;
+    rtUrls: any[];
+    djId: number;
+    copyright: number;
+    s_id: number;
+    mark: number;
+    originCoverType: number;
+    originSongSimpleData?: any;
+    tagPicList?: any;
+    resourceState: boolean;
+    version: number;
+    songJumpInfo?: any;
+    entertainmentTags?: any;
+    awardTags?: any;
+    single: number;
+    noCopyrightRcmd?: any;
+    mv: number;
+    rtype: number;
+    rurl?: any;
+    mst: number;
+    cp: number;
+    publishTime: number;
+    privilege: Privilege;
+}
+
+export interface SingerTopSongResult {
+    code: number;
+    more: boolean;
+    songs: Song[];
+}
+
+// 歌手粉丝
+export interface SingerFansParam {
+    id: number
+    limit?: number
+    offset?: number
+}
+
+
+export interface Associator {
+    vipCode: number;
+    rights: boolean;
+    iconUrl: string;
+}
+
+export interface MusicPackage {
+    vipCode: number;
+    rights: boolean;
+    iconUrl: string;
+}
+
+export interface VipRight {
+    associator: Associator;
+    musicPackage: MusicPackage;
+    redplus?: any;
+    redVipAnnualCount: number;
+    redVipLevel: number;
+}
+
+export interface SingerFansRet {
+    userProfile: profile;
+    vipRights: VipRight;
+}
+
+export interface SingerFansResult {
+    code: number;
+    message: string;
+    data: SingerFansRet[];
+}
