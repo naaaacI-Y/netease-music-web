5a3e3f953e4b49e472a996a701e228d5209627c0
zouyuanle2021 <lrunjie0824@sina.com>
2022-12-17 21:31:03 +0800

    feat: 图片未加载完成默认展示

---
+++ src/components/LazyLoadImg.vue
@@ -0,0 +1,21 @@
+<template>
+    <img :src="defaultImg" alt="" @load="imgLoaded(src)" @error="imgLoadedError">
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+defineProps<{
+    src: string
+}>()
+const defaultImg = ref("/src/assets/images/defaultAvatar.png")
+const imgLoaded = function (src: string) {
+    defaultImg.value = src
+}
+const imgLoadedError = function () {
+    console.log("图片加载失败");
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
