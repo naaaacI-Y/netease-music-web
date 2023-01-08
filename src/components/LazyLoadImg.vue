b50a8101c29e0d20668f33529a87a72d401449dd
zouyuanle2021 <lrunjie0824@sina.com>
2023-01-08 18:07:59 +0800

    feat: 图片加载增加默认展示占位

--- src/components/LazyLoadImg.vue
+++ src/components/LazyLoadImg.vue
@@ -1,21 +1,40 @@
 <template>
-    <img :src="defaultImg" alt="" @load="imgLoaded(src)" @error="imgLoadedError">
+    <div class="img-wrapper">
+        <img :src="defaultImg" alt="" @load="imgLoaded(src)" @error="imgLoadedError" v-if="isShowPic">
+    </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { nextTick, ref, watch } from 'vue';
 
-defineProps<{
+const props = defineProps<{
     src: string
 }>()
-const defaultImg = ref("/src/assets/images/defaultAvatar.png")
+// const isShowBgc = ref(true)
+const isShowPic = ref(true)
+watch(() => props.src, () => {
+    isShowPic.value = false
+    nextTick(() => {
+        isShowPic.value = true
+    })
+})
+const defaultImg = ref("/src/assets/images/defaultImg.png")
 const imgLoaded = function (src: string) {
     defaultImg.value = src
+    // isShowBgc.value = false
 }
 const imgLoadedError = function () {
     console.log("图片加载失败");
 }
 </script>
 <style lang="scss" scoped>
+.img-wrapper {
+    background-color: var(--theme-dc);
+    width: 100%;
 
+    img {
+        width: 100%;
+        // height: 100%;
+    }
+}
 </style>
