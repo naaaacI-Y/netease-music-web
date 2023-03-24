28065ef0b8e063edb9282227f25f906b121a3e3f
zouyuanle2021 <lrunjie0824@sina.com>
2023-03-24 14:58:59 +0800

    feat: 消息通知api初始化

---
+++ src/service/api/message/index.ts
@@ -0,0 +1,42 @@
+import http from "../../http"
+import * as T from "./types"
+
+/**
+ * 通知-私信
+ * @param params limit : 返回数量 , 默认为 30
+ * @param params offset : 偏移数量，用于分页 , 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+ * @returns {PrivateResult}
+ */
+export const getMeesagePrivateInfo = (params: T.QueryParam) => {
+    return http.get<T.PrivateResult>(`/msg/private`, params)
+}
+
+/**
+ * 通知-评论
+ * @param params limit : 返回数量 , 默认为 30
+ * @param params offset : 偏移数量，用于分页 , 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+ * @returns {MessageCommentResult}
+ */
+export const getMessageComment = (params: T.QueryParam & { uid: number }) => {
+    return http.get<T.MessageCommentResult>(`/msg/comments`, params)
+}
+
+/**
+ * 通知- `@我`
+ * @param params limit : 返回数量 , 默认为 30
+ * @param params offset : 偏移数量，用于分页 , 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+ * @returns {MeesageMentionResult}
+ */
+export const getMessageMention = (params: T.QueryParam) => {
+    return http.get<T.MeesageMentionResult>(`/msg/forwards`, params)
+}
+
+/**
+ * 通知-通知
+ * @param params limit : 返回数量 , 默认为 30
+ * @param params offset : 偏移数量，用于分页 , 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+ * @returns {MessageNotificationResult}
+ */
+export const getMessageNotification = (params: T.QueryParam) => {
+    return http.get<T.MessageNotificationResult>(`/msg/notices`, params)
+}
---
+++ src/service/api/message/types.ts
@@ -0,0 +1,20 @@
+export interface QueryParam {
+    limit: number
+    offset: number
+}
+
+export interface PrivateResult {
+
+}
+
+export interface MessageCommentResult {
+
+}
+
+export interface MeesageMentionResult {
+
+}
+
+export interface MessageNotificationResult {
+
+}
